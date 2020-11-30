e12ee905366f8d561fc800fd35d8dcc63c72a165
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-11-30 09:02:44 +0100

    Cool dynamic button

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useEffect } from 'react';
+import React, { useMemo, useEffect, useState } from 'react';
 import './App.css';
 import Button from "@material-ui/core/Button";
 import ScopedCssBaseline from '@material-ui/core/ScopedCssBaseline';
@@ -11,6 +11,10 @@ import { createAppTheme } from "./appTheme";
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import { MyButton } from "./MyButton";
 import { MyButtonBest } from "./MyButtonBest";
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Switch from '@material-ui/core/Switch';
+
+
 
 export function App() {
 
@@ -36,6 +40,8 @@ export function App() {
 
   }
 
+  const [color, setColor] = useState<"red" | "blue">("blue")
+
   return (
     <ThemeProvider theme={theme}>
       <StyledThemeProvider theme={theme}> {/*https://material-ui.com/guides/interoperability/#theme*/}
@@ -45,18 +51,32 @@ export function App() {
 
           <ScopedCssBaseline>
             <>
-            <div>
-              <MyButton color="red">Red</MyButton>
-              <MyButton color="blue">Blue</MyButton>
-              <MyButton color="blue" disabled>Blue disabled</MyButton>
-              <MyButton color="red" disabled>Red disabled</MyButton>
-            </div>
-            <div>
-              <MyButtonBest color="red">Red</MyButtonBest>
-              <MyButtonBest color="blue">Blue</MyButtonBest>
-              <MyButtonBest color="blue" disabled>Blue disabled</MyButtonBest>
-              <MyButtonBest color="red" disabled>Red disabled</MyButtonBest>
-            </div>
+              <div>
+                <MyButton color="red">Red</MyButton>
+                <MyButton color="blue">Blue</MyButton>
+                <MyButton color="blue" disabled>Blue disabled</MyButton>
+                <MyButton color="red" disabled>Red disabled</MyButton>
+              </div>
+              <div>
+                <MyButtonBest color="red">Red</MyButtonBest>
+                <MyButtonBest color="blue">Blue</MyButtonBest>
+                <MyButtonBest color="blue" disabled>Blue disabled</MyButtonBest>
+                <MyButtonBest color="red" disabled>Red disabled</MyButtonBest>
+              </div>
+              <div>
+                <FormControlLabel
+                  style={{ "margin": theme.spacing(2) }}
+                  control={
+                    <Switch
+                      checked={color === "blue"}
+                      onChange={event => setColor(event.target.checked ? "blue" : "red")}
+                      color="primary"
+                    />
+                  }
+                  label={"Switch button color"}
+                />
+                <MyButtonBest color={color}>Dynamic CSS</MyButtonBest>
+              </div>
 
 
               <TypographyTheme>This div's text looks like that of a button.</TypographyTheme>
